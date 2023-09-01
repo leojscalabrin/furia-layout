e748fcaf0643cfa508c39f27a718858a00f3e7e3
Leonardo Scalabrin <leojscalabrin@gmail.com>
2023-09-01 17:53:04 -0300

    vitrine lançamentos

--- js/script.js
+++ js/script.js
@@ -28,64 +28,68 @@ const produtos = [
   },
 ];
 
-function showcaseAssemble(produto) {
-    const showcaseContainer = document.querySelector(".home__showcase");
+function showcaseAssemble(produto, container) {
+  const showcaseContainer = document.querySelector(container);
 
-    const _productWrapper = document.createElement("div");
-    const _productImage = document.createElement("img");
-    const _productName = document.createElement("p");
-    const _discountPrice = document.createElement("p");
-    const _totalPrice = document.createElement("p");
-    const _overlay = document.createElement("div");
-    const _buyButton = document.createElement("button");
-    const _sizeButtons = document.createElement("div");
+  const _productWrapper = document.createElement("div");
+  const _productImage = document.createElement("img");
+  const _productName = document.createElement("p");
+  const _discountPrice = document.createElement("p");
+  const _totalPrice = document.createElement("p");
+  const _overlay = document.createElement("div");
+  const _buyButton = document.createElement("button");
+  const _sizeButtons = document.createElement("div");
 
-    _productWrapper.classList.add("product-card");
-    _productImage.classList.add("product-image");
-    _productName.classList.add("product-name");
-    _discountPrice.classList.add("price");
-    _totalPrice.classList.add("total-price");
-    _overlay.classList.add("overlay");
-    _buyButton.classList.add("buy-button"); 
-    _sizeButtons.classList.add("size-buttons");
+  _productWrapper.classList.add("product-card");
+  _productImage.classList.add("product-image");
+  _productName.classList.add("product-name");
+  _discountPrice.classList.add("price");
+  _totalPrice.classList.add("total-price");
+  _overlay.classList.add("overlay");
+  _buyButton.classList.add("buy-button");
+  _sizeButtons.classList.add("size-buttons");
 
-    _productImage.src = produto.imagem;
-    _productName.textContent = produto.nome;
+  _productImage.src = produto.imagem;
+  _productName.textContent = produto.nome;
 
-    if (produto.precoComDesconto !== undefined) {
-        const discountPrice = parseFloat(produto.precoComDesconto);
-        _discountPrice.textContent = `R$ ${discountPrice.toFixed(2)}`;
+  if (produto.precoComDesconto !== undefined) {
+    const discountPrice = parseFloat(produto.precoComDesconto);
+    _discountPrice.textContent = `R$ ${discountPrice.toFixed(2)}`;
 
-        if (produto.precoTotal !== undefined) {
-            const totalPrice = parseFloat(produto.precoTotal);
-            _totalPrice.textContent = `R$ ${totalPrice.toFixed(2)}`;
-        } else {
-            _totalPrice.style.display = "none";
-        }
+    if (produto.precoTotal !== undefined) {
+      const totalPrice = parseFloat(produto.precoTotal);
+      _totalPrice.textContent = `R$ ${totalPrice.toFixed(2)}`;
+    } else {
+      _totalPrice.style.display = "none";
     }
+  }
 
-    _buyButton.textContent = "Comprar";
+  _buyButton.textContent = "Comprar";
 
-    produto.tamanhos.forEach((tamanho) => {
-        const sizeButton = document.createElement("button");
-        sizeButton.classList.add("size-button");
-        sizeButton.textContent = tamanho;
-        _sizeButtons.appendChild(sizeButton);
-    });
+  produto.tamanhos.forEach((tamanho) => {
+    const sizeButton = document.createElement("button");
+    sizeButton.classList.add("size-button");
+    sizeButton.textContent = tamanho;
+    _sizeButtons.appendChild(sizeButton);
+  });
 
-    _productWrapper.appendChild(_productImage);
-    _productWrapper.appendChild(_productName);
-    _productWrapper.appendChild(_discountPrice);
-    _productWrapper.appendChild(_totalPrice);
+  _productWrapper.appendChild(_productImage);
+  _productWrapper.appendChild(_productName);
+  _productWrapper.appendChild(_discountPrice);
+  _productWrapper.appendChild(_totalPrice);
 
-    _overlay.appendChild(_sizeButtons);
-    _overlay.appendChild(_buyButton);
+  _overlay.appendChild(_sizeButtons);
+  _overlay.appendChild(_buyButton);
 
-    _productWrapper.appendChild(_overlay);
+  _productWrapper.appendChild(_overlay);
 
-    showcaseContainer.appendChild(_productWrapper);
+  showcaseContainer.appendChild(_productWrapper);
 }
 
 produtos.forEach((produto) => {
-  showcaseAssemble(produto);
+  showcaseAssemble(produto, ".home__showcase");
+});
+
+produtos.forEach((produto) => {
+  showcaseAssemble(produto, ".news");
 });
